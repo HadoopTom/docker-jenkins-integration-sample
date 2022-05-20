e6caeed25b2d85c2a949aa5d1dc8be915e2c4a78
Azat <azat.aral@yahoo.com>
2022-05-21 01:43:40 +0600

    We added new stuff

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello World Finally");
+        System.out.println("Hello World Finally Salta added payment");
     }
 }
